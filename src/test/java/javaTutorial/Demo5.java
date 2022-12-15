740330293ff119d34f7f16794b531b1e5f3d8678
kadam <kadam@DESKTOP-C4LC40M>
2022-12-15 22:54:19 +0530

    good team

---
+++ src/test/java/javaTutorial/Demo5.java
@@ -0,0 +1,15 @@
+package javaTutorial;
+
+public class Demo5 {
+
+	public  main() {
+		  final int x = 10;
+		  final double PI = 3.14;
+		 
+		  public static void main(String[] args) {
+		    Main myObj = new Main();
+		    myObj.x = 50;
+		    System.out.println(myObj.x);
+		  }
+		}
+
